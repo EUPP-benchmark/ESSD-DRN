115d27bd91226116219d7c69b28175492b021e88
Jieyu Chen <48775864+jieyu97@users.noreply.github.com>
2022-12-19 12:19:54 +0100

    Update combine_fcst.R

--- output-process/combine_fcst.R
+++ output-process/combine_fcst.R
@@ -1,16 +1,19 @@
 library(feather)
 
-pred_all = read_feather("/Data/eumetnet/eumetnet_temp/new/orog_fcst_leadtime0.feather")
+# specify path
+new_path
+
+pred_all = read_feather(paste0(new_path, "fcst_leadtime0.feather"))
 
 for (n in 1:20) {
   
   print(n)
   
-  path_new = paste0("/Data/eumetnet/eumetnet_temp/new/orog_fcst_leadtime", n, ".feather")
+  path_new = paste0(new_path, "fcst_leadtime", n, ".feather")
   preds = read_feather(path_new)
   
   pred_all = rbind(pred_all, preds)
   
 }
 
-write_feather(pred_all, "/Data/eumetnet/eumetnet_temp/fcst_all_leadtime_v5.feather")
+write_feather(pred_all, paste0(new_path, "fcst_all_leadtime.feather"))
