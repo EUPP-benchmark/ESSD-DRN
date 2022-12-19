09991a1de6eccbab6b4453890d2060092b46d322
Jieyu Chen <48775864+jieyu97@users.noreply.github.com>
2022-12-19 12:13:38 +0100

    Update data_preprocess.py

--- data-preprocess/data_preprocess.py
+++ data-preprocess/data_preprocess.py
@@ -3,27 +3,27 @@ import numpy as np
 import datetime
 
 
-## Preprocessing test data
+## Preprocessing training data
 
 # read data
-test_data = pd.read_feather('/Data/eumetnet/ESSD_benchmark_feather/ESSD_benchmark_test_data.feather')
+train_data = pd.read_feather('/Data/eumetnet/ESSD_benchmark_feather/ESSD_benchmark_training_data.feather')
 
 # remove rows with missing data
-used_data_test = test_data.dropna()
+used_data_train = train_data.dropna()
 
 # remove duplicated columns
-used_data_test = used_data_test.drop(['station_name', 'land_usage', 'altitude', 'latitude', 'longitude'], axis=1)
+used_data_train = used_data_train.drop(['station_name', 'land_usage', 'altitude', 'latitude', 'longitude'], axis=1)
 
 # spilt the dataset by different lead time, and save the files
-df_steps = pd.DataFrame(np.unique(used_data_test['step']))
+df_steps = pd.DataFrame(np.unique(used_data_train['step']))
 
 for k in range(21):
     print(k)
     
-    df_selected = used_data_test[used_data_test['step'] == df_steps[0][k]]
+    df_selected = used_data_train[used_data_train['step'] == df_steps[0][k]]
     data_saved = df_selected.reset_index(drop=True)
 
-    data_saved.to_feather('/Data/eumetnet/eumetnet_temp/new/test_data_leadtime' + str(k) + '.feather') 
+    data_saved.to_feather('/Data/eumetnet/eumetnet_temp/new/train_data_leadtime' + str(k) + '.feather') 
 
 
 ## Preprocessing test data
